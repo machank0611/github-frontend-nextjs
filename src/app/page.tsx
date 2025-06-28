2905f758851fcef90e0a57fb5fcf6769f07ddb27
Machank <87250827+machank0611@users.noreply.github.com>
2025-06-28 15:31:24 +0900

    added func A-1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            Func A-1 Save and see your changes instantly.
           </li>
         </ol>
 
